42314234c5c0f0ec0fdbbd0a8e531b9c16a69309
ananuness <beatriznunez601@gmail.com>
2022-03-23 09:34:44 -0300

    Merge branch 'develop' of github.com:Rogerio-Fidencio/iLocation-front into feature/login_assignment_screen

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ const eyeIcon = document.querySelector('.eye-icon');
 const btnLogin = document.querySelector('.btn-login');
 
 //orders
-const order = document.querySelector('.table tr');
+const orders = document.querySelectorAll('tbody tr');
 const modal = document.querySelector('.order-modal');
 const modalContent = document.querySelector('.modal-content');
+const titleModal = document.querySelector('.modal content h1')
+//const titlesOrder = document.querySelectorAll('td a');
 
 //tracking
+const clientOrder = document.querySelector('.client-order');
 //final
 
 //login
@@ -33,12 +36,19 @@ function submitData(email, password) {
 }
 
 //order
-function showModal() {
+function showModal(e) {
+  //window.location.href = "../tracking/tracking.html";
+  titleModal.textContent = `Pedido nº ${e.target.id}`
   modal.classList.remove('hidden');
 }
 function closeModal() {
   modal.classList.add('hidden');
 }
+
+//tracking
+// function fillHeader(e) {
+//   clientOrder.textContent = `${e.target.textContent}`
+// }
 
 //login
 eyeIcon.addEventListener('click', togglePassword);
@@ -49,6 +59,7 @@ btnLogin.addEventListener('submit', e => {
 });
 
 //order
-order.addEventListener('click', showModal);
+orders.forEach(order => order.addEventListener('click', e => showModal(e)));
 modal.addEventListener('click', closeModal);
 modalContent.addEventListener('click', e => e.stopPropagation());
+//titlesOrder.forEach(title => title.addEventListener('click', e => fillHeader(e)));
